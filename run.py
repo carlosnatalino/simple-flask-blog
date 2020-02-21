43f5fc8a42a662e9e6add8917c147b5dfdf81d92
Carlos Natalino Da Silva <6130746+carlosnatalino@users.noreply.github.com>
2020-02-21 10:42:39 +0100

    Adding params to the run file

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
+import argparse
 from flaskblog import app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('-r', '--reset', action='store_true')
+    parser.add_argument('-p', '--port', default=4000, type=int)
+    parser.add_argument('--host', default='localhost')
+    args = parser.parse_args()
+    print(args)
+
+    if args.reset:  # reset db
+        import loaddatabase
+
+    app.run(debug=args.debug, port=args.port, host=args.host)
